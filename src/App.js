54206a3fb0ef08394cb84f76516a42a5033b5fd6
katlj <kathurnalj@gmail.com>
2024-05-27 14:03:41 -0700

    react bootstrap components work now!!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import Typewriter from "typewriter-effect";
+import React from "react";
+import Headshot from "./Images/Headshot.jpeg";
+import DownArrow from "./Images/DownArrow.png";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import Project from "./Project.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
+
+export default function App() {
+  // Add this in your component file
+  require('react-dom');
+  window.React2 = require('react');
+  console.log(window.React1 === window.React2);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+  
+    <div className="Main ">
+      kathurnasworks
+     <h1>Hi, Im Kathurna!</h1>
+      <h3 id="phonetically"><Typewriter onInit={(typewriter)=> {typewriter.typeString("(kuh-thur")
+      .pauseFor(1000).typeString("-nuh)").start()}}/></h3>
+      <h2 id="subheader">I thrive at the intersection of engineering and design.</h2> 
+  
+     
+       <Container>
+        <Row> 
+        <div className="Bio">
+      <img src={Headshot} alt="Heashot"></img>
+      <p id="minibio"> With my user-centered mindset, I strive to create innovative,
+       creative, and game changing solutions that bring about tangible change.</p>
+     </div></Row>
+     </Container>
+     
+    <div className="Move">
+     
+     <img src={DownArrow} alt="Move down"></img>
+     </div>
+    <div className="Projects">My Projects</div>
+    <Project/>
+    <div className="Socials">Let's Connect!</div>
+    
     </div>
+    </>
   );
 }
-
-export default App;
---
+++ src/Project.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import "./Project.css";
+
+
+
+export default function Project() {
+    return (
+      <>
+      <Card>
+      <Card.Body>THis is my card</Card.Body>
+     </Card>
+      </>
+    );
+  }
